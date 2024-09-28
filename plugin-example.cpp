d74341d236aeaef15d01d0e5186b8ad72f87dbee
borz7zy <borz7zy@yandex.ru>
2024-09-28 14:46:27 +0200

    add export registration of native functions

--- plugin-example.cpp
+++ plugin-example.cpp
@@ -6,9 +6,16 @@ public:
     PluginExample()
     {
     }
+
     ~PluginExample()
     {
     }
+
+    Native_Function_List *RegisterNatives()
+    {
+        return const_cast<Native_Function_List *>(native_list);
+    }
+
     static int native_testFunc(lua_State *L)
     {
         lua_pushstring(L, "Hello from PluginExample!");
@@ -20,3 +27,8 @@ private:
         {"TestFunction", native_testFunc},
         {nullptr, nullptr}};
 } pluginExample;
+
+extern "C" __attribute__((visibility("default"))) Native_Function_List *RegisterNatives()
+{
+    return pluginExample.RegisterNatives();
+}
